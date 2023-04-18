fcca197f9d14b0f56baea98359623b69f3f53297
peknight <JKpeknight@gmail.com>
2023-04-18 18:07:03 +0800

    update

--- random-core/shared/src/main/scala/com/peknight/random/id/LinearCongruentialRandom.scala
+++ random-core/shared/src/main/scala/com/peknight/random/id/LinearCongruentialRandom.scala
@@ -4,7 +4,7 @@ import cats.Id
 import com.peknight.random.Random as Rand
 import com.peknight.random.algorithm.LinearCongruential
 
-private[random] case class LinearCongruentialRandom(seed: Long) extends Random:
+case class LinearCongruentialRandom(seed: Long) extends Random:
   def next(bits: Int): Id[(Rand[Id], Int)] =
     val (nextSeed, r) = LinearCongruential.next(seed, bits)
     (LinearCongruentialRandom(nextSeed), r)
--- random-core/shared/src/main/scala/com/peknight/random/monad/LinearCongruentialRandom.scala
+++ random-core/shared/src/main/scala/com/peknight/random/monad/LinearCongruentialRandom.scala
@@ -4,7 +4,7 @@ import cats.{Applicative, Monad}
 import com.peknight.random.Random as Rand
 import com.peknight.random.algorithm.LinearCongruential
 
-private[random] case class LinearCongruentialRandom[F[_] : Monad](seed: Long) extends Random[F]:
+case class LinearCongruentialRandom[F[_] : Monad](seed: Long) extends Random[F]:
   def next(bits: Int): F[(Rand[F], Int)] =
     val (nextSeed, r) = LinearCongruential.next(seed, bits)
     Applicative[F].pure((LinearCongruentialRandom(nextSeed), r))
