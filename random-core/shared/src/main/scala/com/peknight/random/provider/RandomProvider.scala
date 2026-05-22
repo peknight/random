ec6cabe4feaa5c7f00d035d646832c08a19f6983
peknight <JKpeknight@gmail.com>
2026-05-22 15:45:44 +0800

    update

--- random-core/shared/src/main/scala/com/peknight/random/provider/RandomProvider.scala
+++ random-core/shared/src/main/scala/com/peknight/random/provider/RandomProvider.scala
@@ -5,15 +5,16 @@ import cats.effect.std.AtomicCell
 import cats.syntax.functor.*
 import com.peknight.random.Random
 import com.peknight.random.algorithm.LinearCongruential
+import com.peknight.random.monaderror.LinearCongruentialRandom
 
 trait RandomProvider[F[_]]:
   def random: F[Random[F]]
 end RandomProvider
 object RandomProvider:
-  java.util.Random()
   def of[F[_]: Async](random: Long => Random[F]): F[RandomProvider[F]] =
     AtomicCell[F].of[Long](LinearCongruential.seedUniquifier).map(seedUniquifier =>
       LinearCongruentialRandomProvider(seedUniquifier)(random)
     )
+  def linearCongruential[F[_]: Async]: F[RandomProvider[F]] = of[F](LinearCongruentialRandom[F])
   def apply[F[_]](using provider: RandomProvider[F]): RandomProvider[F] = provider
 end RandomProvider
